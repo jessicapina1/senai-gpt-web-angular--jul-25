272206077bff72c58c6faee584596288105427c0
jessicapina1 <jessicapinalima@gmail.com>
2025-09-18 21:50:09 -0300

    conexao com gemini

--- src/app/chat-screen/chat-screen.component.ts
+++ src/app/chat-screen/chat-screen.component.ts
@@ -45,6 +45,7 @@ export class ChatScreenComponent {
     this.getChats();
 
   }
+
   async getChats() { //metodo que busca os chats da API
     let response = await firstValueFrom(this.http.get("https://senai-gpt-api.azurewebsites.net/chats", {
       headers: {
@@ -52,7 +53,7 @@ export class ChatScreenComponent {
       }
 
 
-    }))
+    }));
 
     if (response) {
 
@@ -99,5 +100,53 @@ export class ChatScreenComponent {
       userId: localStorage.getItem("meuId"),
       text: this.mensagemUsuario.value
     };
+
+    //1- salva as mensagens do usuario no banco de dados.
+    let novaMensagemUsuarioResponse = await firstValueFrom(this.http.post("https://senai-gpt-api.azurewebsites.net/messages", novaMensagemUsuario, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("meuToken")
+      }
+
+    }));
+
+    await this.onChatClick(this.chatSelecionado); //atualiza as mensagens da tela
+
+    //2- enviar a mensagem do usuario para a IA responder
+    let respostaIAResponse = await firstValueFrom(this.http.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent", {
+      "contents": [ {
+        "parts": [
+          {
+            "text": this.mensagemUsuario.value + ". Me dê uma resposta objetiva."
+          }
+        ]
+      }
+    ]
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-goog-api-key": "AIzaSyDV2HECQZLpWJrqCKEbuq7TT5QPKKdLOdo"
+      }
+    })) as any;
+
+    let novaRespostaIA = {
+
+      chatId: this.chatSelecionado.id,
+      userId: "chatbot",
+      text: respostaIAResponse.candidates[0].content.parts[0].text,
+      //id a api gera
+    }
+
+    //3- salva a resposta da IA no banco de dados
+    let novaRespostaIAResponse = await firstValueFrom(this.http.post("https://senai-gpt-api.azurewebsites.net/messages", novaRespostaIA, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("meuToken")
+      }
+
+    }));
+
+    await this.onChatClick(this.chatSelecionado);
+
   }
 }
